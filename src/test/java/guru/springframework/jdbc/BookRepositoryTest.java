4d168039e5969b08a414bcddae7ceb9d47fd6945
helen <hhma@alumni.cpp.edu>
2022-10-11 18:06:59 -0400

    use isGreaterThanOrEqualTo  for book count.

--- src/test/java/guru/springframework/jdbc/BookRepositoryTest.java
+++ src/test/java/guru/springframework/jdbc/BookRepositoryTest.java
@@ -71,7 +71,7 @@ public class BookRepositoryTest {
             count.incrementAndGet();
         });
 
-        assertThat(count.get()).isGreaterThan(5);
+        assertThat(count.get()).isGreaterThanOrEqualTo(5);
     }
 
     @Test
